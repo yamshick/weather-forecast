035b79f693f50d69e879b1697298806b0beafcb8
danila <danila.it@rambler.ru>
2023-01-16 22:10:20 +0300

    added new cities to dictionary

--- src/dictionaries/cities.js
+++ src/dictionaries/cities.js
@@ -35,6 +35,11 @@ export const dictCities = [
     long: "13.144554",
   },
   {
+    name: "Hamburg",
+    lat: "53.5582447",
+    long: "9.6476445",
+  },
+  {
     name: "Minsk",
     lat: "53.8845585",
     long: "27.4532863",
@@ -69,7 +74,6 @@ export const dictCities = [
     lat: "50.855103",
     long: "4.3053775",
   },
-
   {
     name: "Porto",
     lat: "41.1621376",
@@ -101,10 +105,126 @@ export const dictCities = [
     lat: "40.6971494",
     long: "-74.2598661",
   },
-
   {
     name: "Philadelphia",
     lat: "40.0022072",
     long: "-75.2581542",
+  },
+  {
+    name: "Detroit",
+    lat: "42.3526257",
+    long: "-83.2392889",
+  },
+  {
+    name: "Pittsburg",
+    lat: "40.4313473",
+    long: "-80.0505405",
+  },
+  {
+    name: "Atlanta",
+    lat: "33.7672884",
+    long: "-84.5602156",
+  },
+  {
+    name: "Dallas",
+    lat: "32.8205865",
+    long: "-96.8716377",
+  },
+  {
+    name: "Nashville",
+    lat: "36.186314",
+    long: "-87.065433",
+  },
+  {
+    name: "Phoenix",
+    lat: "33.6050991",
+    long: "-112.4052399",
+  },
+  {
+    name: "Los Angeles",
+    lat: "34.0201613",
+    long: "-118.6919212",
+  },
+  {
+    name: "San Diego",
+    lat: "32.8242404",
+    long: "-117.3891677",
+  },
+  {
+    name: "San Jose",
+    lat: "37.2958797",
+    long: "-121.9561808",
+  },
+  {
+    name: "San Francisco",
+    lat: "37.7576793",
+    long: "-122.5076401",
+  },
+  {
+    name: "Las Vegas",
+    lat: "36.1249185",
+    long: "-115.3150848",
+  },
+  {
+    name: "Miami",
+    lat: "25.7823907",
+    long: "-80.2994988",
+  },
+  {
+    name: "Calgary",
+    lat: "51.0272883",
+    long: "-114.3680137",
+  },
+
+  {
+    name: "Quito",
+    lat: "-0.1865938",
+    long: "-78.5706253",
+  },
+  {
+    name: "Bogota",
+    lat: "4.6482837",
+    long: "-74.247894",
+  },
+  {
+    name: "Buenos Aires",
+    lat: "-34.6158037",
+    long: "-58.5033386",
+  },
+  {
+    name: "Montovideo",
+    lat: "-34.821018",
+    long: "-56.3765236",
+  },
+  {
+    name: "Rio De Janeiro",
+    lat: "-22.9140693",
+    long: "-43.5860682",
+  },
+
+  {
+    name: "Istambul",
+    lat: "41.0049823",
+    long: "28.7319945",
+  },
+  {
+    name: "Dubai",
+    lat: "25.0757595",
+    long: "54.9472954",
+  },
+  {
+    name: "New Delhi",
+    lat: "28.5272803",
+    long: "77.0688995",
+  },
+  {
+    name: "Kaboul",
+    lat: "34.5533869",
+    long: "68.9175426",
+  },
+  {
+    name: "Haifa",
+    lat: "32.7996175",
+    long: "34.9817565",
   },
 ];
